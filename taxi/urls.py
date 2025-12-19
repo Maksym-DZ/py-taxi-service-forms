0c4cdd741f30ba67e474b9ea64c197c5436bdff2
Maksym Dazidov <maksymdazidov@gmail.com>
2025-12-19 16:45:13 +0100

    forms 2

--- taxi/urls.py
+++ taxi/urls.py
@@ -26,8 +26,8 @@ urlpatterns = [
     path("cars/create/", CarCreateView.as_view(), name="car-create"),
     path("manufacturers/<int:pk>/update/", ManufacturerUpdateView.as_view(), name="manufacturer-update"),
     path("cars/<int:pk>/update/", CarUpdateView.as_view(), name="car-update"),
-    path("manufacturers/<int:pk>/delete", ManufacturerDeleteView.as_view(), name="manufacturer-delete"),
-    path("cars/<int:pk>/delete", CarDeleteView.as_view(), name="car-delete"),
+    path("manufacturers/<int:pk>/delete/", ManufacturerDeleteView.as_view(), name="manufacturer-delete"),
+    path("cars/<int:pk>/delete/", CarDeleteView.as_view(), name="car-delete"),
 ]
 
 app_name = "taxi"
