705a58139dd9a9e623c7e66fcc94ad5c340b341c
Maksym Dazidov <maksymdazidov@gmail.com>
2025-12-19 16:56:59 +0100

    forms 3

--- taxi/views.py
+++ taxi/views.py
@@ -82,10 +82,8 @@ class CarUpdateView(LoginRequiredMixin, generic.UpdateView):
 class ManufacturerDeleteView(LoginRequiredMixin, generic.DeleteView):
     model = Manufacturer
     success_url = reverse_lazy("taxi:manufacturer-list")
-    template_name = "taxi/manufacturer_confirm_delete.html"
 
 
 class CarDeleteView(LoginRequiredMixin, generic.DeleteView):
     model = Car
     success_url = reverse_lazy("taxi:car-list")
-    template_name = "taxi/car_confirm_delete.html"
